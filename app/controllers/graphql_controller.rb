ea7de00b9e76b6665cbb65bba1034475fb453b9d
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-03-13 16:54:54 -0300

    ready api

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -6,6 +6,7 @@ class GraphqlController < ApplicationController
     context = {
       current_identity: current_identity
     }
+
     result = AskalfredApiSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue => e
