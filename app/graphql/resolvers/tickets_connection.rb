4bcd56e16da9e5d6f47d032bdb1448886c54bc95
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-05-10 18:06:09 -0400

    custom tickets connection type

--- app/graphql/resolvers/tickets_connection.rb
+++ app/graphql/resolvers/tickets_connection.rb
@@ -1,6 +1,22 @@
+class TicketsEdgeType < GraphQL::Types::Relay::BaseEdge
+  node_type(Types::Ticket)
+end
+
+module Types
+  class TicketsConnection < GraphQL::Types::Relay::BaseConnection
+    field :total_count, Integer, null: false
+
+    def total_count
+      object.nodes.size
+    end
+
+    edge_type(TicketsEdgeType)
+  end
+end
+
 module Resolvers
   class TicketsConnection < Resolvers::BaseResolver
-    type Types::Ticket.connection_type, null: false
+    type Types::TicketsConnection, null: false
 
     def resolve
       current_identity.tickets
