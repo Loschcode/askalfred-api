914f817ba03af9521591c07319baf10525e937da
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-05-22 19:14:09 -0400

    fix validation

--- app/models/event_message.rb
+++ app/models/event_message.rb
@@ -3,6 +3,4 @@ class EventMessage < ActiveRecord::Base
   accepts_nested_attributes_for :event
 
   validates :body, presence: true, allow_blank: false
-  # this checks for printable character which's way better
-  validates_format_of :body, :with => /\A[[:graph:]]\Z/i
 end
