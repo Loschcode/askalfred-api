7d62c8c001c28ae8de9690b3150794213b492807
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-06-03 17:15:51 -0400

    checks cancellation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  config.action_controller.default_url_options = { host: ENV['API_URL'] }
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
