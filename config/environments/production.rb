82e6b93b5ded7d6acc98054f5e5444c576cc8d97
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-06-04 18:15:49 -0400

    protect forgery done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://api.askalfred.app/cable'
+  config.action_cable.disable_request_forgery_protection = false
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
