aeadf7e55585286ca11b6f3c5135515ec132ade3
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-06-04 16:20:39 -0400

    default url change

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  # default_url_options host: ENV['APP_URL']
+
   mount Sidekiq::Web => '/sidekiq'
 
   ActiveAdmin.routes(self)
-  default_url_options host: ENV['APP_URL']
 
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
