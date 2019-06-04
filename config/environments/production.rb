f57851958bdc26d6e62d2ee619cddc6434e2c684
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-06-04 19:21:11 -0400

    config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,10 +34,10 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
-  config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://api.askalfred.app/cable'
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://api.askalfred.app'
+  # config.action_cable.allowed_request_origins = ['https://askakfred.app']
   config.action_cable.disable_request_forgery_protection = false
-  config.action_cable.allowed_request_origins = ['https://askakfred.app']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
