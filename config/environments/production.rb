a5fd123dbce49e76528c3d2a6f25432490ce912f
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-06-04 18:10:24 -0400

    pumpa config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
+  config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://api.askalfred.app/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
