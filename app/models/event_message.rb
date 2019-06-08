4a91059e648b7e28a222ab81294f64a2e3a0a367
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-06-08 00:58:07 -0400

    bugfix

--- app/models/event_message.rb
+++ app/models/event_message.rb
@@ -1,6 +1,5 @@
 class EventMessage < ActiveRecord::Base
-  belongs_to :event, -> { where(events: {eventable_type: 'EventMessage'}) }, foreign_key: 'eventable_id'
-  # has_one :event, as: :eventable
+  has_one :event, as: :eventable
   accepts_nested_attributes_for :event
 
   validates :body, presence: true, allow_blank: false
