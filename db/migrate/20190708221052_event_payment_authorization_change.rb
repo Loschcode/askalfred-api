f28ff5cf1fa2d21ee7f17267b6114144fd45ceba
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-07-09 15:50:37 -0400

    fix column migration

--- db/migrate/20190708221052_event_payment_authorization_change.rb
+++ db/migrate/20190708221052_event_payment_authorization_change.rb
@@ -1,6 +1,6 @@
 class EventPaymentAuthorizationChange < ActiveRecord::Migration[5.2]
   def change
     rename_column :event_payment_authorizations, :stripe_charge_id, :stripe_payment_intent_id
-    rename_column :credits, :stripe_intent_id, :stripe_payment_intent_id
+    # rename_column :credits, :stripe_intent_id, :stripe_payment_intent_id
   end
 end
