0601ad57f9896e1f07334d0f2a9d3c400a82f2e8
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-11-10 05:26:33 +0100

    remove traces

--- app/workers/dispatch_subscription_worker.rb
+++ app/workers/dispatch_subscription_worker.rb
@@ -6,13 +6,11 @@ class DispatchSubscriptionWorker
   sidekiq_options queue: 'critical'
 
   def perform(channel, arguments, response, scope)
-    TrackingService::Slack.new(Identity.first).track('DISPATCH SUBSCRIPTION WORKER', {'random': true})
     AskalfredApiSchema.subscriptions.trigger(
       channel,
       arguments,
       RedisTransmissionService.deserialize(response),
       scope.deep_symbolize_keys
     )
-    TrackingService::Slack.new(Identity.first).track("END DISPATCH SUBSCRIPTION WORKER", { 'random': true })
   end
 end
